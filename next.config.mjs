b57177d0258fa6774c6fab6719d12c4c99135c81
Thabo Tankiso Thebe <thabo.tankiso.thebe@gmail.com>
2026-09-22 10:40:36 +0200

    build: configure static export with configurable basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH !== undefined ? process.env.NEXT_PUBLIC_BASE_PATH : '/marka',
   reactStrictMode: true,
 };
 
